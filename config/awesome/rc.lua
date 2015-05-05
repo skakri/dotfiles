cc65578748ff4b891780a921228b58d0bdb1a36f
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2015-05-05 08:05:35 +0000

    Test: disabled menu, tag switching on root window.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -293,11 +293,12 @@ end
 -- }}}
 
 -- {{{ Mouse bindings
-root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
+-- Test: disabled.
+-- root.buttons(awful.util.table.join(
+--     awful.button({ }, 3, function () mymainmenu:toggle() end),
+--     awful.button({ }, 4, awful.tag.viewnext),
+--     awful.button({ }, 5, awful.tag.viewprev)
+-- ))
 -- }}}
 
 -- {{{ Key bindings
