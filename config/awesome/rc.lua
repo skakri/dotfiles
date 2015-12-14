4ef656309834ef6c94b40d0a47f80757243010ff
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2015-12-14 16:10:10 +0200

    Awesome WM layout adjustments

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -69,6 +69,8 @@ local layouts =
 {
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
