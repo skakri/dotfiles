fd58687d0ae2a99864ba7ddfc2f3818257af7e57
Kristaps Karlsons <kristaps.karlsons@gmail.com>
2016-09-27 13:22:50 +0300

    Awesome: disable mod-shift-q "quit" action

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -336,7 +336,7 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    -- awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
     awful.key({modkey,            }, "F1",     function () awful.screen.focus(1) end),
     awful.key({modkey,            }, "F2",     function () awful.screen.focus(2) end),
