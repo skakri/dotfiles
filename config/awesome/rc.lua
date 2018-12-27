64d86b5b2be3da400cf7da873b02a8d1012ca7eb
Kristaps Karlsons <kristaps.karlsons@intelligentsystems.lv>
2018-12-27 15:12:19 +0000

    Add rofi

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -324,6 +324,8 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey,           }, "Home", function () awful.spawn("rofi -show ssh -modi ssh") end,
+              {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
